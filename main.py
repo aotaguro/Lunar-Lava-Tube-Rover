450eebd490b3f7c8f74ebb64147e522de73c28fc
aotaguro <149980719+aotaguro@users.noreply.github.com>
2026-07-13 08:53:56 -0700

    bug fix?

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from direct.showbase.ShowBase import ShowBase
 class LavaTubeSim(ShowBase):
    #constructer and ininitalizing panda3D 
     def __init__(self): 
-        ShowBase.__init_(self)
+        ShowBase.__init__(self)
 
         self.lavaTube = self.loader.loadModel("assets/lunarTube.glb")
         self.lavaTube.reparentTo(self.render)
