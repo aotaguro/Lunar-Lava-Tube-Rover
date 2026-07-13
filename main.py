b77a858bee115d47421ea1e656f1603847c4a6ac
aotaguro <149980719+aotaguro@users.noreply.github.com>
2026-07-13 08:41:58 -0700

    hopefully cave is displayed

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from direct.showbase.ShowBase import ShowBase 
+class LavaTubeSim(ShowBase):
+   #constructer and ininitalizing panda3D 
+    def __init__(self): 
+        ShowBase.__init_(self)
+
+        self.lavaTube = self.loader.loadModel("assets/lunarTube.glb")
+        self.lavaTube.reparentTo(self.render)
+        self.lavaTube.setPos(0,0,0)
+        self.lavaTube.setScale(1)
+        self.disableMouse()
+        self.camera.setPos(0,-25, 8)
+        self.camera.lookAt(0,0,0)
+
+app = LavaTubeSim()
+app.run()
 
